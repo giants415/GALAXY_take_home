e64bffd5511c73c3153d3d4405e469646dad8c12
Andrew <andrew.vinocur@gmail.com>
2016-10-25 16:06:26 -0700

    basic requirements for nav bar

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,10 @@
 console.log ("app.js connected");
 
 $(document).ready(function (){
-  $("#session-status").append('<li>Log In</li><li style="display:none">Log Out</li>');
-  $("#session-status").click(function(){
+   $(".button-collapse").sideNav();
+
+  $(".session-status").append('<li>Log In</li><li style="display:none">Log Out</li>');
+  $(".session-status").click(function(){
     $('li', this).toggle();
   });
 });
